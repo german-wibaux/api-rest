0fcf1c8943bab8fe1adf4fa78ea9116d683285cc
German <German>
2018-03-29 10:32:19 -0300

    Fix Procfile

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port : process.env.PORT || 3001,
-    db: 'mongodb://heroku_s95ksjn7:octubre18*@ds127129.mlab.com:27129/heroku_s95ksjn7' 
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/properties'
 }
